001b25c4209cf7e0381da2bc412180061d1181fb
Eric J Fisher <EricJFIsher@Live.com>
2017-12-21 15:14:33 -0500

    Test StandardGradeBook doesn't set Type

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -3,5 +3,8 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
+        public StandardGradeBook(string name) : base(name)
+        {
+        }
     }
 }
