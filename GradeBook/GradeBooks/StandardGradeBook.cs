d9b0d8b0ea19b2caeaedb78c114dfe90262f658f
Eric J Fisher <EricJFIsher@Live.com>
2017-12-21 15:23:43 -0500

    Testing with StandardGradeBook task completed

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 namespace GradeBook.GradeBooks
 {
-    public class StandardGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
     }
 }
