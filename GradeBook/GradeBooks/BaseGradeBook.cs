57718aad2b38d5d3fde3f4826698e72ae583ecea
Eric J Fisher <EricJFIsher@Live.com>
2017-12-21 14:30:15 -0500

    Test BaseGradeBook.Type setter is private

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -13,7 +13,7 @@ namespace GradeBook.GradeBooks
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
-        public GradeBookType Type { private get; set; }
+        public GradeBookType Type { get; private set; }
 
         public BaseGradeBook(string name)
         {
